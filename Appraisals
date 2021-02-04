8417fefe9a501381b2d5c95d7c901060defeea97
Reid Morrison <reidmo@gmail.com>
2021-02-04 14:58:34 -0500

    So many different combinations of Rails and Ruby ...

--- Appraisals
+++ Appraisals
@@ -7,7 +7,9 @@ appraise "rails_4.2" do
 end
 
 appraise "rails_5.0" do
-  gem "activerecord", "~> 5.0"
+  gem "activerecord", "~> 5.0.0"
+  gem "activerecord-jdbcsqlite3-adapter", "~> 1.0", platform: :jruby
+  gem "sqlite3", "~> 1.3.6", platform: :ruby
 end
 
 appraise "rails_5.1" do
--- lib/active_record_replica/extensions.rb
+++ lib/active_record_replica/extensions.rb
@@ -5,18 +5,49 @@ module ActiveRecordReplica
   module Extensions
     extend ActiveSupport::Concern
 
-    %i[select select_all select_one select_rows select_value select_values].each do |select_method|
+    no_keyword_args = %i[select select_one select_rows select_value select_values]
+    keyword_args = []
+
+    if ActiveRecord::VERSION::MAJOR >= 5
+      keyword_args << :select_all
+    else
+      no_keyword_args << :select_all
+    end
+
+    no_keyword_args.each do |select_method|
+      class_eval <<~RUBY, __FILE__, __LINE__ + 1
+        def #{select_method}(*args)
+          return super if active_record_replica_read_from_primary?
+
+          active_record_replica_select(:#{select_method}, *args)
+        end
+      RUBY
+    end
+
+    keyword_args.each do |select_method|
       class_eval <<~RUBY, __FILE__, __LINE__ + 1
         def #{select_method}(*args, **kargs)
           return super if active_record_replica_read_from_primary?
 
-          active_record_replica_select(:#{select_method}, *args, **kargs)
+          active_record_replica_select_kargs(:#{select_method}, *args, **kargs)
         end
       RUBY
     end
 
     if ActiveRecord::VERSION::MAJOR >= 6
-      def active_record_replica_select(select_method, sql, name = nil, *args, **kargs)
+      def active_record_replica_select(select_method, sql, name = nil, *args)
+        ActiveRecordReplica.read_from_primary do
+          if ActiveRecord::Base.current_role == ActiveRecord::Base.reading_role
+            public_send(select_method, sql, "Replica: #{name || 'SQL'}", *args)
+          else
+            ActiveRecord::Base.connected_to(role: ActiveRecord::Base.reading_role) do
+              reader_connection.public_send(select_method, sql, "Replica: #{name || 'SQL'}", *args)
+            end
+          end
+        end
+      end
+
+      def active_record_replica_select_kargs(select_method, sql, name = nil, *args, **kargs)
         ActiveRecordReplica.read_from_primary do
           if ActiveRecord::Base.current_role == ActiveRecord::Base.reading_role
             public_send(select_method, sql, "Replica: #{name || 'SQL'}", *args, **kargs)
@@ -35,6 +66,12 @@ module ActiveRecordReplica
       def active_record_replica_select(select_method, sql, name = nil, *args)
         ActiveRecordReplica.read_from_primary do
           reader_connection.public_send(select_method, sql, "Replica: \#{name || 'SQL'}", *args)
+        end
+      end
+
+      def active_record_replica_select_kargs(select_method, sql, name = nil, *args, **kargs)
+        ActiveRecordReplica.read_from_primary do
+          reader_connection.public_send(select_method, sql, "Replica: \#{name || 'SQL'}", *args, **kargs)
         end
       end
 
